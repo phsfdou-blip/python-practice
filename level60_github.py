3aab1f36fd9aaf5a2f533a29d64cf8bcf086833a
phsfdou-blip <phsfdou@gmail.com>
2026-05-06 18:17:11 -0700

    Update level60  with age input

--- level60_github.py
+++ level60_github.py
@@ -1,4 +1,7 @@
 name = input("Enter your name: ")
+age = input("Enter your age: ")
 
 print(f"Hello {name}!")
+print(f"You are {age} years old. ")
+print("Level 60 upgraded! ")
 print("GitHub leve 60 win win win! ")
