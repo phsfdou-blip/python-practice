f83cda181eec0ab6c1cd58a66e34e9d895bb7884
phsfdou-blip <phsfdou@gmail.com>
2026-06-29 13:10:45 -0700

    Add Level76 Return Function practice

---
+++ level76_return_square.py
@@ -0,0 +1,11 @@
+def square(number):
+    return number * number
+
+name = input("Enter your name: ")
+number = int(input("Enter a number: "))
+
+result = square(number)
+
+print("Hello", name)
+print("Square:", result)
+print("Great job learning Python! 🚀")
